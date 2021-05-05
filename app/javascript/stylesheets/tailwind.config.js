a5e165fce0730f865e4ce0bf85f04c73cf8b4993
yuichiyasui <yassan.b.a.w@gmail.com>
2021-05-05 23:26:49 +0900

    タスク一覧の表示処理を実装

--- app/javascript/stylesheets/tailwind.config.js
+++ app/javascript/stylesheets/tailwind.config.js
@@ -1,11 +1,12 @@
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
-}
+	purge: [],
+	darkMode: false, // or 'media' or 'class'
+	theme: {
+		extend: {},
+	},
+	variants: {
+		backgroundColor: ["odd", "hover"],
+		extend: {},
+	},
+	plugins: [],
+};
